22730a12f93a8c242dca8ebd53dc100b43057320
Mia <ngocmhuynh5@gmail.com>
2018-06-06 09:55:19 -0700

    fizzbuzz solution

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,15 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    const arr = [];
+    for (let i = 1; i <= num; i++) {
+    if (i % 3 == 0) console.log('fizz');
+    else if (i % 5 == 0) console.log('buzz');
+    else if (i % 15 == 0) console.log('fizzbuzz');
+    else console.log(i);
+    arr.push(i);
+    }
+    return arr;
 }
 
 module.exports = fizzbuzz;
