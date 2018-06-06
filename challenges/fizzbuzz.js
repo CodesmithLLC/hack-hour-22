654e910fb85355fcb1f828fda3afeff7c5a64254
Adam Modras <25adam@gmail.com>
2018-06-06 10:03:25 -0700

    completed 1st HackHour
    Finished first hack hour and completed it 3 different ways.  Commented out the other approaches to this problem.

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,39 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+  const output = [];
 
+  for (let i = 1; i <= num; i++) {
+    let str = '';
+    if (i % 3 === false || (!(i % 3)) || i % 3 === 0) {
+      str += 'fizz';
+    }
+    if (i % 5 === 0) {
+      str += 'buzz';
+    }
+    output.push(str || i);
+
+    // i % 15 === 0 ? output.push('fizzbuzz') :
+    //  i % 5 === 0 ? output.push('buzz') :
+    //   i % 3 === 0 ? output.push ('fizz') :
+    //     output.push(i);
+
+    //   if (i % 15 === 0) {
+    //     output.push('fizzbuzz');
+    //   }
+    //   if (i % 5 === 0 ) {
+    //     output.push('buzz');
+    //   }
+    //   if (i % 3 === 0) {
+    //     output.push('fizz');
+    //   }
+    //   else {
+    //     output.push(i);
+    //   }
+  }
+  return output;
 }
+
+console.log(fizzbuzz(16));
 
 module.exports = fizzbuzz;
