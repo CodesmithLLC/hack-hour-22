fd0774977e29f76199eae3ac3d2606a9427571d2
Jung Shin <crnboii@gmail.com>
2018-06-09 09:18:58 -0700

    finished mode-mean

---
+++ challenges/mode-mean.js
@@ -0,0 +1,42 @@
+/*
+ * Given an array of numbers, determine if the mode and mean of the array are equivalent
+ *
+ * Caveats:
+ *  - Math.floor the mean
+ *  - If there are multiple modes, use the max of the modes
+ *
+ * Return true or false
+ *
+ */
+
+
+function modemean(array) {
+  let obj = array.reduce((a,b) => {
+              a[b] ? a[b] ++ : a[b] = 1 
+              return a;
+            } , {})
+  let arrAvg = array.reduce((a,b) => a + b, 0) / array.length
+  let mode;
+  let arrOfTally = []
+  
+  for(let key in obj){
+    arrOfTally.push(obj[key])
+  }
+  let highest = Math.max(...arrOfTally);
+  
+  for(let key in obj){
+    if(highest === obj[key]){
+      mode = key;
+    }
+  }
+  
+  if(Number(mode) === Number(arrAvg)){
+    return true;
+  }
+  return false;
+}
+
+const test = [1,1,1]
+console.log(modemean(test))
+
+module.exports = modemean;
