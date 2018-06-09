527b5a4d5a2c4b2f41907d4b2a13208c59503dc0
Brendan Morrell <brendanmorrell@gmail.com>
2018-06-09 09:52:27 -0700

    did a few versions. this one is ugly but I accidentally deleted the others. will try to recreate if time permits

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -8,10 +8,24 @@
  * Return true or false
  *
  */
-
-
 function modemean(array) {
-
+  if (!array.length) {
+    return true;
+  }
+  let highest = [0, 0];
+  let mean = array.reduce((acc, num) => acc + num) / array.length;
+  let count = array.reduce((acc, x) => {
+    if (acc[x]) acc[x] = acc[x] += 1;
+    if (!acc[x]) acc[x] = 1;
+    return acc;
+  }, {});
+  for (const key in count) {
+    if (count[key] > highest[1] || (count[key] === highest[1] && +key > highest[0])) {
+      highest = [+key, count[key]];
+    }
+  }
+  return mean === highest[0];
 }
 
+console.log(modemean([3, 4, 5, 5, 8]))
 module.exports = modemean;
