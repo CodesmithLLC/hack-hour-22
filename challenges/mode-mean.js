45a842578664255f65f842c23180c48e842860d8
briantaylor <brian.taylor818@gmail.com>
2018-06-09 09:57:21 -0700

    submission for modemean HackHour

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,41 @@
 
 
 function modemean(array) {
+  let meanFound;
+  let modeFound;
+  const mean = (arr) => {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+      sum += array[i];
+    }
+    meanFound = sum / arr.length;
+  }
 
+  // find the mode of the array
+  const getMode = (arr) => {
+    let mode;
+    let most = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+      let currentEle = arr[i];
+      let currCount = 0;
+      for(let j = 0; j < arr.length; j++) {
+        if(arr[i] === arr[j]) {
+          currCount++;
+        }
+      }
+      if(currCount > most) {
+        most = currCount;
+        mode = currEle;
+      }
+    }
+    return modeFound;
+  }
+
+  if(meanFound === modeFound) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = modemean;
