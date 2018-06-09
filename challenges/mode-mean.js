7fb56995b29d0da09c2473560326ec70de6c3df4
nathanielgrossman <nategrossman@mac.com>
2018-06-09 09:43:44 -0700

    completed mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,27 @@
 
 
 function modemean(array) {
-
+	if (array.length === 0) return true;
+	let occurs = {};
+	let mode;
+	let mean = Math.floor((array.reduce((acc, val) => {return acc + val})) / array.length)
+	array.forEach(num => {
+		if (occurs[num]) {
+			occurs[num] += 1;
+		} else {
+			occurs[num] = 1;
+		}
+	})
+	for(let key in occurs) {
+		if (occurs[key] >= occurs[mode] || mode === undefined) {
+			mode = key
+		}
+	}
+	console.log(mean);
+	console.log(mode);
+	return mode == mean; 
 }
 
 module.exports = modemean;
+
+console.log(modemean(arr))
