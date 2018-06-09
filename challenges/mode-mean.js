c6c68a023c551be24c3ce09b06ef63c065f0941d
Adam Modras <25adam@gmail.com>
2018-06-09 09:41:37 -0700

    completed modemean challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,34 @@
  *
  */
 
-
 function modemean(array) {
 
+  function findMean(array) {
+    if (array.length === 0) return 0;
+    const newArray = [...array];
+    const total = newArray.reduce((curr, acc) => curr += acc);
+    const mean = Math.floor(total / newArray.length);
+    return mean;
+  }
+
+  function findMode(array, mostUsed = 0) {
+    if (array.length === 0) return 0;
+    const newArray = [...array];
+    const numbers = {};
+    let mode;
+    newArray.forEach((num) => {
+      numbers[num] ? numbers[num] += 1 : numbers[num] = 1;
+    })
+    for (const key in numbers) {
+      if (numbers[key] >= mostUsed) {
+        mode = parseInt(key);
+        mostUsed = numbers[key];
+      }
+    }
+    return mode;
+  }
+  return findMean(array) === findMode(array);
 }
 
+console.log(modemean([]));
 module.exports = modemean;
