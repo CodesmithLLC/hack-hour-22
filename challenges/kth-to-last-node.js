d2aed50acf0fc6bd17cb6f280e2ce53313dba474
Blessing Ebowe <blessingebowe@gmail.com>
2018-06-12 09:43:20 -0700

    Remove testing

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -33,16 +33,3 @@ function kthToLastNode(k, head) {
 }
 
 module.exports = { Node, kthToLastNode };
-
-const a = new Node('A');
-const b = new Node('B');
-const c = new Node('C');
-const d = new Node('D');
-const e = new Node('E');
-
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
-
-console.log(kthToLastNode(3, a));
