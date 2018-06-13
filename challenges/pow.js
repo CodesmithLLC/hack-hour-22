083693efb91a729b4da417c340c6e31fcd0f462a
Ryan Trontz <rtrontz@gmail.com>
2018-06-13 09:52:14 -0700

    snarky integer exponent restriction message

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,8 @@
  */
 
 function pow(base, power) {
-  if (power === 0) return 1;
+  if (parseInt(power) !== power) return 'only integer exponents, please. this is javascript after all.'
+  if (power === 0 || power === -0) return 1;
   if (power > 0) {
     return power === 1 ? base : base * pow(base, power - 1);
   }
@@ -12,7 +13,8 @@ function pow(base, power) {
     let result = power === 1 ? base : base * pow(base, power - 1);
     return 1 / result;
   }
-
 } 
 
 module.exports = pow;
+
+
