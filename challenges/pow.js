3fd83802a7e268a8a1d53057924e75de50cd36f3
Cam <>
2018-06-13 09:29:33 -0700

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,12 @@
  */
 
 function pow(base, power) {
+    if ( base === 1 ) return 1;
+    if ( base === 0 ) return 0;
+    return ( power > 0 ) ? base * pow ( base, power - 1 ) : 1;
 
 }
+
+console.log( pow(2, 4) );
 
 module.exports = pow;
