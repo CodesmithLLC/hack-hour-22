b94ed2996e3eb28d0f1047dbc87e9385ed04f60b
nathanielgrossman <nategrossman@mac.com>
2018-06-13 09:57:20 -0700

    completed pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,23 @@
  * Use recursion!
  */
 
-function pow(base, power) {
 
-}
+
+function pow(base, power, output = 1) {
+	switch (power) {
+		case -1: // base case for negative exponent
+			return 1 / (output * base);
+		case 0: //return 1 if called with 0 as power
+			return 1;
+		case 1: //base case for positive exponent
+			return output * base;
+		default:
+			power > 0 ? power-- : power++; //if running with positive exponent, decrement. if negative, increment
+			output = output * base;
+			return pow(base, power, output)
+	}
+}  
+
 
 module.exports = pow;
+
