e5cebe15f998905b98511dbf341904ed5df3d66b
Stephanie Fong <stfongg@gmail.com>
2018-06-15 09:46:55 -0700

    completed challenge

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,11 @@
  */
 
 function reverseInPlace(array) {
-
+    let result = [];
+    for (let i = array.length - 1; i >= 0 ; i--) {
+        result.push(array[i]);
+    }
+    return result;
 }
 
 module.exports = reverseInPlace;
