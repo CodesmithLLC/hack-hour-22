76cdd0006d134812d40bb2f924e631b9486e8210
nathanielgrossman <nategrossman@mac.com>
2018-06-15 09:57:33 -0700

    completed reverse in place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,13 @@
  */
 
 function reverseInPlace(array) {
-
+	let middle = Math.floor(array.length / 2);
+	for (let i = 0; i < middle; i++) {
+		let temp = array[i]
+		array[i] = array[array.length - (i+1)];
+		array[array.length - (i+1)] = temp;
+	}
+	return array;
 }
 
 module.exports = reverseInPlace;
