1db581b60ec667a0b6b972a9a263385927c25faa
Clariz Mariano <clariz.mariano@gmail.com>
2018-06-16 10:00:48 -0700

    string-rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -12,11 +12,44 @@
  */
 
 function isSubstring(s1, s2) {
+  let doubledString = s1.concat(s1);
+  console.log(doubledString);
   return s1.indexOf(s2) >= 0;
 }
 
 function stringRotation(s1, s2) {
+  
+  if (s1.length !== s2.length) {return false};
+  // check if char of s2 is equals to the s1's index, index+1 or index-1
 
+  for (let i = 0; i < s1.length; i++ ) {
+    let charAtIdx = s1[i];
+    let charAfter, charBefore;
+
+    if (i === s1.length -1) {
+      charAfter = s1[0];
+    }
+    else {
+      charAfter = s1[i + 1];
+    }
+    if (i === 0) {
+      charBefore = s1[s1.length -1];
+    }
+    else {
+      charBefore = s1[i - 1];
+    }
+    console.log(s2[i], "=", charAtIdx, charAfter, charBefore)
+
+    if (s2[i] === charAfter || s2[i] === charBefore || s2[i] === charAtIdx  ) {
+    }
+    else {
+      return false
+    }
+  }
+
+  
+  
+  return true;
 }
-
+console.log(stringRotation("hello", "ohell"));
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
