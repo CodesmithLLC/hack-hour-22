d6bdb2cb33f358abc7306a39b78411569468fe71
Joe Thel <jgraythel@gmail.com>
2018-06-16 09:50:39 -0700

    🎉 (string-rotation): rotate strings

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,7 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+  return s1.length === s2.length ? isSubstring(s1 + s1, s2) : false;
 }
 
-module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+module.exports = { isSubstring, stringRotation };
