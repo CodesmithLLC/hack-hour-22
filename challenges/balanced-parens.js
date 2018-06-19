2bcb5cc382cf2b9644c37246c33dc26cfbde1a2e
Blessing Ebowe <blessingebowe@gmail.com>
2018-06-19 15:38:04 -0700

    Solution 3 for balanced parens.

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,26 +25,61 @@
  */
 
 function balancedParens(input) {
-  const brackets = ['{', '}', '[', ']', '(', ')'];
+  // SOLUTION 1
+  // const brackets = ['{', '}', '[', ']', '(', ')'];
+  // const bracketsObj = { '{': '}', '[': ']', '(': ')' };
+  // const containedBrackets = input.split('').filter(char => brackets.includes(char));
+  // const { length } = containedBrackets;
+  // if ((length % 2) > 0) return false;
+  // while (containedBrackets.length > 1) {
+  //   const elem = containedBrackets[0];
+  //   if (bracketsObj[elem] === containedBrackets[1]) {
+  //     containedBrackets.shift();
+  //     containedBrackets.shift();
+  //     break;
+  //   } else if (bracketsObj[elem] === containedBrackets[length - 1]) {
+  //     containedBrackets.shift();
+  //     containedBrackets.pop();
+  //     break;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  // return true;
+
+  // SOLUTION 2
+  // const opening = ['{', '[', '('];
+  // const closing = ['}', ']', ')'];
+  // const bracketStack = [];
+
+  // const inputArr = input.split('');
+  // for (let index = 0; index < inputArr.length; index += 1) {
+  //   const element = inputArr[index];
+  //   const openingIndex = opening.indexOf(element);
+  //   const closingIndex = closing.indexOf(element);
+  //   if (openingIndex > -1) bracketStack.push(openingIndex);
+  //   if ((closingIndex > -1) && (closingIndex !== bracketStack.pop())) return false;
+  // }
+  // return !(bracketStack.length > 0);
+
+  // SOLUTION 3
   const bracketsObj = { '{': '}', '[': ']', '(': ')' };
-  const containedBrackets = input.split('').filter(char => brackets.includes(char));
-  const { length } = containedBrackets;
-  if ((length % 2) > 0) return false;
-  while (containedBrackets.length > 1) {
-    const elem = containedBrackets[0];
-    if (bracketsObj[elem] === containedBrackets[1]) {
-      containedBrackets.shift();
-      containedBrackets.shift();
-      break;
-    } else if (bracketsObj[elem] === containedBrackets[length - 1]) {
-      containedBrackets.shift();
-      containedBrackets.pop();
-      break;
-    } else {
-      return false;
-    }
+  const bracketStack = [];
+
+  const inputArr = input.split('');
+  for (let index = 0; index < inputArr.length; index += 1) {
+    const element = inputArr[index];
+    const closingBracket = bracketsObj[element];
+    if (closingBracket) bracketStack.push(closingBracket);
+    if (element.match(/[}\])]/) && (element !== bracketStack.pop())) return false;
   }
-  return true;
+  return !(bracketStack.length > 0);
 }
 
 module.exports = balancedParens;
+
+// console.log(balancedParens('[](){}'));
+// console.log(balancedParens('[(]{)}'));
+// console.log(balancedParens('[({})]'));
+// console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }'));
+// console.log(balancedParens(' var hubble = function() { telescopes.awesome();'));
