ff442e8e9e979908e3c14fd0932f3dc66bfb0d60
Sam Goldberg <sgoldber61@gmail.com>
2018-06-19 09:31:43 -0700

    balanced parens first draft

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,46 @@
  *
  */
 
-function balancedParens(input){
-
+function balancedParens(input) {
+  const stack = [];
+  
+  for (const char of input) {
+    switch (char) {
+      case '(':
+        stack.push('(');
+        break;
+      case '[':
+        stack.push('[');
+        break;
+      case '{':
+        stack.push('{');
+        break;
+      case ')':
+        if (stack.pop() !== '(') return false;
+        break;
+      case ']':
+        if (stack.pop() !== '[') return false;
+        break;
+      case '}':
+        if (stack.pop() !== '{') return false;
+        break;
+    }
+  }
+  
+  return stack.length === 0;
 }
 
+// console.log(balancedParens('('));  // false
+// console.log(balancedParens('()')); // true
+// console.log(balancedParens(')('));  // false
+// console.log(balancedParens('(())'));  // true
+// 
+// console.log(balancedParens('[](){}')); // true
+// console.log(balancedParens('[({})]'));   // true
+// console.log(balancedParens('[(]{)}')); // false
+// 
+// console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+// console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
+
 module.exports = balancedParens;
+
