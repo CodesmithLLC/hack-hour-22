9fd251ff1ecac6f91e0ca4a0c6a1ac495a860efd
Brendan Morrell <brendanmorrell@gmail.com>
2018-06-19 09:31:26 -0700

    finished balance parens.

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -1,31 +1,37 @@
-/*
- * write a function that takes a string of text and returns true if
- * the parentheses are balanced and false otherwise.
- *
- * Example:
- *   balancedParens('(');  // false
- *   balancedParens('()'); // true
- *   balancedParens(')(');  // false
- *   balancedParens('(())');  // true
- *
- * Step 2:
- *   make your solution work for all types of brackets
- *
- * Example:
- *  balancedParens('[](){}'); // true
- *  balancedParens('[({})]');   // true
- *  balancedParens('[(]{)}'); // false
- *
- * Step 3:
- * ignore non-bracket characters
- * balancedParens(' var wow  = { yo: thisIsAwesome() }'); // true
- * balancedParens(' var hubble = function() { telescopes.awesome();'); // false
- *
- *
- */
-
-function balancedParens(input){
-
+function balancedParens(input) {
+  return !input.split('').reduce((acc, char) => {
+    switch (true) {
+      case char === '(':
+        acc.push(char);
+        break;
+      case char === '{':
+        acc.push(char);
+        break;
+      case char === '[':
+        acc.push(char);
+        break;
+      case char === ')' && acc[acc.length - 1] === '(':
+        acc.pop();
+        break;
+      case char === '}' && acc[acc.length - 1] === '{':
+        acc.pop();
+        break;
+      case char === ']' && acc[acc.length - 1] === '[':
+        acc.pop();
+        break;
+      default:
+        break;
+    }
+    return acc;
+  }, []).length;
 }
 
+console.log(balancedParens('(')); // false
+console.log(balancedParens('()')); // true
+console.log(balancedParens(')(')); // false
+console.log(balancedParens('(())')); // true
+
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]')); // true
+console.log(balancedParens('[(]{)}')); // false
 module.exports = balancedParens;
