9d5c1bd93406a559f4f7e84d62080fbb8e43a5d9
Adam Modras <25adam@gmail.com>
2018-06-20 10:05:50 -0700

    updated roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,42 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
 function romanNumeral(n) {
+  const numeralMap = [
+    1000, 'M',
+    900, 'CM',
+    500, 'D',
+    400, 'CD',
+    100, 'C',
+    90, 'XC',
+    50, 'L',
+    40, 'XL',
+    10, 'X',
+    9, 'IX',
+    5, 'V',
+    4, 'IV',
+    1, 'I'
+  ];
 
+  if (n < 1) return 'nulla';
+  // let value = '';
+  const romanArray = [];
+  for (let i = 0; i < numeralMap.length; i += 2) {
+    while (n >= numeralMap[i]) {
+      console.log(n);
+      console.log(numeralMap[i + 1]);
+      romanArray.push(numeralMap[i + 1]);
+      // value += numeralMap[i + 1];
+      n -= numeralMap[i];
+    }
+  }
+  return romanArray.join('');
+  // return value;
 }
+
+console.log(romanNumeral(78));
 
 module.exports = romanNumeral;
