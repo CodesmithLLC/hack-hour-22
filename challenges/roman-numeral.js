9f2f519069759df0d0953b98dbf6883e67cdc172
Cam <>
2018-06-20 10:01:40 -0700

    roman numerals draft

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -19,6 +19,38 @@
 
 function romanNumeral(n) {
 
+    const setLevelValues = [1, 5, 10];
+    const setLevelSymbols = ["I","V","X"];
+
+    let outputString = "";
+
+    const recurseRomanNumerals = function ( num ) {
+        if ( num > 0 ) {
+            let newVal;
+            for ( let i = setLevelValues.length - 1; i > -1; i-- )
+            {
+                // check special situations
+                // handle the IV and IX and XL type situations
+                // didnt finish this part
+
+                if ( num >= setLevelValues[i] ) {
+                    outputString = outputString + setLevelSymbols[i];
+                    newVal = num - setLevelValues[i];
+                    break;
+                }
+            }
+            recurseRomanNumerals( newVal );
+        }
+    }
+
+    recurseRomanNumerals( n );
+
+    return outputString;
 }
+console.log(romanNumeral(4));   //this outputs IIII
+console.log(romanNumeral(5));   //this outputs V
+console.log(romanNumeral(6));   //this outputs VI
+console.log(romanNumeral(7));   //this outputs VII
+console.log("end");
 
 module.exports = romanNumeral;
