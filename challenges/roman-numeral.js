9702f8568d6fdb36ed986e26b927b923d15d0b0e
nathanielgrossman <nategrossman@mac.com>
2018-06-20 10:00:57 -0700

    finished roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,20 @@
  */
 
 function romanNumeral(n) {
-
+	if (!n > 0) return '';
+	const numerals = [[1,'I'],[4, 'IV'],[5,'V'],[9, 'IX'],[10, 'X'],[50, 'L'],[90, 'XC'],[100, 'C'], [400, 'CD'], [500, 'D'],[900, 'CM'],[1000, 'M']];
+	for (let i = 0; i < numerals.length; i++) {
+		if (numerals[i][0] === n) return numerals[i][1];
+		else if (numerals[i][0] > n) {
+			n = n - numerals[i-1][0];
+			return numerals[i-1][1] + romanNumeral(n);
+		}
+	}
+	n = n - numerals[numerals.length - 1][0];
+	return numerals[numerals.length - 1][1] + romanNumeral(n);
 }
 
-module.exports = romanNumeral;
+
+//module.exports = romanNumeral;
+
+
