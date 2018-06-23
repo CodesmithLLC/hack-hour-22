c3a24049496453716b64c2b9ac76119f857bc7e2
Mia <ngocmhuynh5@gmail.com>
2018-06-23 10:06:23 -0700

    completed challenge

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,11 @@
  */
 
 function binToDec(binary) {
-
+    // let digit = parseInt(binary, 2);
+    // return digit;
+    return binary.split('').reverse().reduce((x, y, i) => {
+        return (y === '1') ? x + Math.pow(2, i) : x;
+    }, 0);
 }
 
 module.exports = binToDec;
