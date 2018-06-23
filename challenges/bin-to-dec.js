19b204737370e859a90da4e486b506d9a3eab6bc
Sam Goldberg <sgoldber61@gmail.com>
2018-06-23 09:29:55 -0700

    binary first draft

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,21 @@
  */
 
 function binToDec(binary) {
-
+  let mult = 1;
+  let result = 0;
+  for (let i = binary.length - 1; i >= 0; i--) {
+    result += parseInt(binary.charAt(i)) * mult;
+    mult *= 2;
+  }
+  
+  return result;
 }
+
+// console.log(binToDec('0'));//   -> 0
+// console.log(binToDec('11'));//  -> 3
+// console.log(binToDec('100'));// -> 4
+// console.log(binToDec('101'));// -> 5
+// console.log(binToDec('0101'));// -> 5
+// console.log(binToDec('101111001'));// -> 377
 
 module.exports = binToDec;
