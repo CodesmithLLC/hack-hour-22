f417e4835e838a9dd12af7107bac71bdc6f432b1
Sam Goldberg <sgoldber61@gmail.com>
2018-06-26 09:52:09 -0700

    match word first draft

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,27 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
-
+  const wordsString = str.replace(/[^A-Za-z]+/g, ' ').toLowerCase().trim();
+  const words = wordsString ? wordsString.split(' ') : [];
+  
+  const wordStack = [];
+  for (const word of words) {
+    const reversed = word.split('').reverse().join('');
+    
+    if (wordStack[wordStack.length - 1] === reversed)
+      wordStack.pop();
+    else
+      wordStack.push(word);
+  }
+  
+  return wordStack.length === 0;
 }
+
+// console.log(matchWord('__END_DNE-----')); // -> true
+// console.log(matchWord('__ENDDNE__')); // -> false       (not separated by a space)
+// console.log(matchWord('IF()()fi[]')); // -> true        (should be case-insensitive)
+// console.log(matchWord('for__if__rof__fi')); // -> false     not properly closed. like ( [) ] 
+// console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw')); // -> true
+// console.log(matchWord('')); // -> true
 
 module.exports = matchWord;
