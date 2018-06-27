69de7e03522855933127e33c633082352ceca4a3
Sam Goldberg <sgoldber61@gmail.com>
2018-06-27 09:45:28 -0700

    highest product first draft

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,27 @@
  */
 
 function highestProduct(array) {
-
+  array.sort((a, b) => b - a);
+  
+  if (array.length === 0)
+    return 1;
+  else if (array.length === 1)
+    return array[0];
+  else if (array.length === 2)
+    return array[0] * array[1];
+  else
+    return Math.max(array[0] * array[1] * array[2], array[0] * array[array.length - 2] * array[array.length - 1]);
 }
+
+
+// console.log(highestProduct([40, 100, 1, 5, 25, 10]));
+// console.log(highestProduct([-40, 100, 1, 5, 25, 10]));
+// 
+// console.log(highestProduct([-40, -1, 5, 10]));
+// console.log(highestProduct([-40, -1, 5, -10]));
+// 
+// console.log(highestProduct([-40, -1, -5, -10]));
+
 
 
 module.exports = highestProduct;
