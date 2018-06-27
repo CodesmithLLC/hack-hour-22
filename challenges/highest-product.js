65be65a30988c084dccfcdba89a9d7aff575dcd8
Ryan Trontz <rtrontz@gmail.com>
2018-06-27 09:40:22 -0700

    didn't know you wanted zero for a short array

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,18 +3,17 @@
  */
 
 function highestProduct(array) {
-  if (array.length <= 3) return array.reduce((acc, num) => acc *= num);
+  const multiplier = arr => arr.reduce((acc, num) => acc *= num);
+
+  if (array.length <= 3 || !Array.isArray(array)) return 0;
 
   const negatives = array.filter(x => x < 0).sort((a,b) => a - b);
   const sortedArr = array.sort((a,b) => b - a);
 
-  const multiplier = arr => arr.reduce((acc, num) => acc *= num);
 
-  if (negatives.length <= 1) {
-    return multiplier(sortedArr.slice(0, 3))
-  } else {
-    return multiplier(negatives.slice(0, 2).concat(sortedArr[0]));
-  }
+  return (negatives.length <= 1 ?
+    multiplier(sortedArr.slice(0, 3)) :
+    multiplier(negatives.slice(0, 2).concat(sortedArr[0])));
 }
 
 
