93abc020777658ff6de01d669421d3d7612a9392
briantaylor <brian.taylor818@gmail.com>
2018-06-27 09:48:20 -0700

    submission for highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,7 +4,21 @@
 
 function highestProduct(array) {
 
-}
+  // sort numbers and multiply the three largest integers in the array together
+  let numsArr = [];
+  if(array.length > 3 || Array.isArray(array)) return 0;
 
+  // sort array into order from least to greatest
+  const sortArr = array.sort((x, y) => {
+    return x - y;
+  })
+
+  for(let i = 0; i <= 1; i++) {
+    if(Math.abs(sortArr[i]) > sorrArr[sortArr.length - 1]) {
+      numsArr.push(sortArr[i]);
+    }
+  }
+  numsArr.push(sorted[sorted.length - 1]);
+}
 
 module.exports = highestProduct;
