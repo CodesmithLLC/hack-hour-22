eeabc7d00b471934cb4dbbdc563a47d448074c96
Blessing Ebowe <blessingebowe@gmail.com>
2018-06-27 09:58:21 -0700

    Update highestprod soln

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,19 +4,19 @@
 
 function highestProduct(array, maxProduct = 0) {
   let product = maxProduct;
-  if (array.length > 3) {
-    const firstMultiplier = array[0];
-    for (let index = 1; index < array.length; index += 1) {
-      const secondMultiplier = array[index];
-      for (let jdex = index + 1; jdex < array.length; jdex += 1) {
-        const thirdMultiplier = array[jdex];
-        product = Math.max(product, (firstMultiplier * secondMultiplier * thirdMultiplier));
-      }
-    }
-    return highestProduct(array.slice(1), product);
+  if (!(array instanceof Array) || array.length < 3) {
+    return product;
   }
+  const firstMultiplier = array[0];
+  for (let index = 1; index < array.length; index += 1) {
+    const secondMultiplier = array[index];
+    for (let jdex = index + 1; jdex < array.length; jdex += 1) {
+      const thirdMultiplier = array[jdex];
+      product = Math.max(product, (firstMultiplier * secondMultiplier * thirdMultiplier));
+    }
+  }
+  highestProduct(array.slice(1), product);
   return product;
 }
-
 
 module.exports = highestProduct;
