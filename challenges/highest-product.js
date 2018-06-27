55fe55b653aec4a400d68aabde1e795ba8653d5d
nathanielgrossman <nategrossman@mac.com>
2018-06-27 09:41:40 -0700

    completed highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,23 @@
  */
 
 function highestProduct(array) {
-
+	if (array.length < 3) return 'error';
+	array.sort((a, b) => {
+		return a-b;
+	});
+	let hi = array.slice(-3);
+	let hiLo = array.slice(0, 2)
+	hiLo.push(hi[2])
+	if (hi.reduce(product) > hiLo.reduce(product)) return hi.reduce(product);
+	else return hiLo.reduce(product);
 }
 
+function product(a, b) {
+	return a * b;
+}
 
-module.exports = highestProduct;
+module.exports = { 
+	highestProduct,
+	product
+};
+
