bd7597c4c96fdff220484f809a71d9ec62f6ded7
Mia <ngocmhuynh5@gmail.com>
2018-06-29 18:12:02 -0700

    completed zip-linked-list challenge

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,31 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+ // make a new head pointer to an empty linked linst 
+ // check the first value of both linked lists 
+ // whichever node from L1/L2 is smaller, append it to the new list and move the pointer ot the next node
+ // reach the end of the list
+
+  // create a new linked list pointer 
+  let l3 = new Node(null, null);
+  let previous = l3;
+
+  //while both linked lists aren't empty
+  while (l1 !== null && l2 !== null) {
+    if (l1.value <= l2.value) {
+      previous.next = l1;
+      l1 = l1.next;
+    } else {
+      previous.next = l2;
+      l2 = l2.next;
+    }
+    previous = previous.next;
+  }
+  // once we reach end of a linked list, append the other list because it's sorted
+  if (l1 === null) { previous.next = l2; }
+  if (l2 === null) { previous.next = l1 }
+
+  return l3.next;
 };
 
 module.exports = {Node: Node, zip: zip};
