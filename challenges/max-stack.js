af54b044429cf6398092eff06fcae7af84692003
nathanielgrossman <nategrossman@mac.com>
2018-06-30 09:49:48 -0700

    max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,32 @@
  */
 
 function Stack() {
-  // body...
+	this.storage = {};
+	this.index = 0;
+
+	this.push = (val) => {
+		this.storage[this.index] = val;
+		this.index ++;
+		return this.index;
+	}
+
+	this.pop = () => {
+		if (this.index === 0) return;
+		this.index --;
+		let temp = this.storage[this.index];
+		delete this.storage[this.index];
+		return temp;
+	}
+
+	this.getMax = () => {
+		if (this.index === 0) return;
+		let max = this.storage[0];
+		for (let i = 0; i < this.index; i++) {
+			if (max < this.storage[i]) max = this.storage[i];
+		}
+		return max;
+	}
 }
+
 
 module.exports = Stack;
