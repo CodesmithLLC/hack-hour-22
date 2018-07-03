2514aeb38ff947a7fbe0e74ccf226f78542f2ab4
Blessing Ebowe <blessingebowe@gmail.com>
2018-07-03 09:45:44 -0700

    Complete Reversed Linked List

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,20 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
 function reverseLinkedList(head) {
-
+  if ((!head || !head.next)) return head;
+  let temp = head;
+  while (temp.next) {
+    let temp2 = head.next;
+    head = head.next;
+    temp.next = temp2.next;
+    head.next = temp;
+  }
+  return head;
 }
 
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+module.exports = { Node, reverseLinkedList };
