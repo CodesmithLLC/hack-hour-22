6bca8b2542c8370f5307521d22c4ca9bc59dec8c
Brendan Morrell <brendanmorrell@gmail.com>
2018-07-03 09:57:03 -0700

    this is a horrible solution. no idea what is wrong with me today. it does work, though

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -8,13 +8,38 @@
  *
  */
 
+const LL = new Node(1);
+LL.next = new Node(2);
+LL.next.next = new Node(3);
+LL.next.next.next = new Node(4);
+LL.next.next.next.next = new Node(5);
+
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
 function reverseLinkedList(head) {
-
+  const valuesArr = [];
+  function grabValues(LL) {
+    if (!LL) return;
+    if (LL) valuesArr.push(LL.value);
+    grabValues(LL.next);
+  }
+  grabValues(head);
+  const reversedVals = valuesArr.reverse().concat(null);
+  const newLL = new Node(reversedVals[0]);
+  function addValToLL(node, i = 1) {
+    const value = reversedVals[i];
+    if (value) {
+      node.next = new Node(value);
+      addValToLL(node.next, i + 1);
+    } else {
+      node.next = value;
+    }
+  }
+  addValToLL(newLL);
+  return newLL;
 }
-
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+console.log(reverseLinkedList(LL));
+module.exports = { Node, reverseLinkedList };
