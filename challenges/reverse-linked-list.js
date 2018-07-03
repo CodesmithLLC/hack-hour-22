d366ebffe975ba6787838ae9b6e9c6620af3aa8b
nathanielgrossman <nategrossman@mac.com>
2018-07-03 10:00:55 -0700

    rll

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,25 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
 
-function reverseLinkedList(head) {
-
+function reverseLinkedList(head, original = null) {
+	if (head === null) return;
+  if (original === null) original = head;
+  let current = head;
+  while (current !== original) {
+    current = current.next;
+  }
+  if (current.next === null) {
+    return head;
+  }
+  let temp = current.next.next;
+  let newH = current.next;
+  newH.next = head;
+  current.next = temp
+  return reverseLinkedList(newH, original);
 }
 
-module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
+module.exports = { Node: Node, reverseLinkedList: reverseLinkedList };
