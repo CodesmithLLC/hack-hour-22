86de7553392b124b2311c127fa78201c97f24b34
Sam Goldberg <sgoldber61@gmail.com>
2018-07-03 09:58:20 -0700

    reverse linked list first draft

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -9,12 +9,76 @@
  */
 
 function Node(value) {
-    this.value = value;
-    this.next = null;
+  this.value = value;
+  this.next = null;
 }
+
+// nonrecursive version
+
+// function reverseLinkedList(head_) {
+//   let curr = head_, head, result = null;
+//   while (curr) {
+//     head = curr.next;
+//     curr.next = result;
+//     result = curr;
+//     curr = head;
+//   }
+// 
+//   return result;
+// }
+
+// recursive version
 
 function reverseLinkedList(head) {
-
+  if (!(head && head.next)) return head;
+  
+  const newTail = head.next;
+  head.next = null;
+  
+  const newHead = reverseLinkedList(newTail);
+  newTail.next = head;
+  return newHead;
 }
+
+/*
+// log the contents of a list
+function logger(list) {
+  console.log('begin log');
+  
+  let node = list;
+  while (node) {
+    console.log(node.value);
+    node = node.next;
+  }
+}
+
+// test 1
+(() => {
+  const l = new Node(2);
+  l.next = new Node(3);
+  l.next.next = new Node(4);
+  l.next.next.next = new Node(9);
+  
+  logger(reverseLinkedList(l));
+})();
+
+// test 2
+(() => {
+  const l = new Node(0);
+  l.next = new Node(3);
+  l.next.next = new Node(4);
+  l.next.next.next = new Node(7);
+  l.next.next.next.next = new Node(12);
+  
+  logger(reverseLinkedList(l));
+})();
+
+// test 3
+(() => {
+  const l = new Node(2);
+  
+  logger(reverseLinkedList(l));
+})();
+*/
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
