2deb9f0f5154362f2ab1d874118aa2662a9d7820
cssaavedra <cssaavedra56@gmail.com>
2018-07-07 09:57:09 -0700

    common-elements and perm-palin hack hours

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,35 @@
  */
 
 function permPalin(str) {
-	
+    let array = str.split('');
+    let obj = {
+      "once": 0
+      };
+    for (let i = 0; i < array.length; i++){
+      if (!(array[i] in obj)){
+        obj[array[i]] = 1;
+        }
+      else {
+        obj[array[i]] += 1;
+        }  
+      }
+    switch(obj){
+      
+      case(array.length % 2 === 0):
+        for(const prop in obj){
+          if (obj[prop] % 2 !== 0) return false
+          }
+          return true;
+      default: 
+        for(const prop in obj){
+          if (obj[prop] % 2 !== 0) {
+            if (obj.once === 0){
+              obj.once += 1;
+              }
+            else {return false;} }
+          }
+          return true;
+      }  
 }
 
 module.exports = permPalin;
