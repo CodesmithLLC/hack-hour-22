a3c2ac2e50c79f39a2e5b713815513d9667dc1b7
briantaylor <brian.taylor818@gmail.com>
2018-07-07 10:00:46 -0700

    submission for perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,18 @@
  *
  */
 
-function permPalin(str) {
-	
+const permPalin = (str) => {
+  let lcount = {}
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    letter = str[i];
+    lcount[letter] = lcount[letter] || 0;
+    lcount[letter]++;
+  }
+	for (let counter in lcount) {
+    sum += lcount[counter] % 2;
+  }
+  return sum < 2;
 }
 
 module.exports = permPalin;
