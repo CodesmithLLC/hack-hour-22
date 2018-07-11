906f71a73f04c73c684723c8ee52b57a4c9d7a5c
Joe Thel <jgraythel@gmail.com>
2018-07-11 09:30:06 -0700

    ✨ stack-queue
    Definitely the most efficient solution for queueing!

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,8 +4,24 @@
 
 
 function Stack() {
-
+  this.contents = {};
+  this.length = 0;
 }
+
+Stack.prototype.push = function push(val) {
+  this.contents[this.length] = val;
+  this.length += 1;
+  return this.length;
+};
+
+Stack.prototype.pop = function pop() {
+  if (this.length === 0) return undefined;
+
+  const temp = this.contents[this.length - 1];
+  delete this.contents[this.length - 1];
+  this.length -= 1;
+  return temp;
+};
 
 
 /**
@@ -14,7 +30,26 @@ function Stack() {
 
 
 function Queue() {
-
+  this.inStack = new Stack();
+  this.outStack = new Stack();
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+Queue.prototype.enqueue = function enqueue(val) {
+  this.inStack.push(val);
+  return this.inStack.length;
+};
+
+Queue.prototype.dequeue = function dequeue() {
+  if (this.inStack.length === 0) return undefined;
+
+  while (this.inStack.length > 0) {
+    this.outStack.push(this.inStack.pop());
+  }
+  const temp = this.outStack.pop();
+  while (this.outStack.length > 0) {
+    this.inStack.push(this.outStack.pop());
+  }
+  return temp;
+};
+
+module.exports = { Stack, Queue };
