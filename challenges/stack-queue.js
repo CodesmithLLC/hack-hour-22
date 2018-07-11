1304e3562ff6aac3e911ac5dff6470bf6f5c0793
nathanielgrossman <nategrossman@mac.com>
2018-07-11 09:41:10 -0700

    sq

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,19 @@
 
 
 function Stack() {
+ 	this.storage = {};
+ 	this.index = 0;
 
+ 	this.push = (val) => {
+ 		this.storage[this.index] = val;
+ 		return ++this.index;
+ 	}
+
+ 	this.pop = () => {
+ 		let temp = this.storage[--this.index];
+ 		delete this.storage[this.index];
+ 		return temp;
+ 	}
 }
 
 
@@ -14,7 +26,28 @@ function Stack() {
 
 
 function Queue() {
+	this.in = new Stack();
+	this.out = new Stack();
+	this.length = 0;
+
+	this.enqueue = (val) => {
+		this.in.push(val);
+		return ++this.length;
+	}
+
+	this.dequeue = () => {
+		while(this.in.index > 0) {
+			this.out.push(this.in.pop());
+		}
+		let temp = this.out.pop();
+		while(this.out.index > 0) {
+			this.in.push(this.out.pop());
+		}
+		this.length--;
+		return temp;
+	}
 
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
+
