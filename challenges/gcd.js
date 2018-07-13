5b137db5a632199fc801b5129dcc1e326ba3c378
Brendan Morrell <brendanmorrell@gmail.com>
2018-07-13 09:33:33 -0700

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,16 @@
  */
 
 function gcd(a, b) {
-
+  let gcd = Math.min(a, b);
+  while (gcd) {
+    if (!(a % gcd || b % gcd)) break;
+    gcd -= 1;
+  }
+  return gcd;
 }
+
+console.log(gcd(10, 8));
+console.log(gcd(10, 9));
+console.log(gcd(10, 5));
 
 module.exports = gcd;
