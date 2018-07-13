1e42041a185c295e62c60233021a52799c1191f2
Clariz Mariano <clariz.mariano@gmail.com>
2018-07-13 10:00:29 -0700

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,24 @@
  */
 
 function gcd(a, b) {
+    const big = a > b ? a : b;
+    const small = a < b ? a : b;
+    const limit = Math.sqrt(big);
+    let arr = [];
+// find what the numbers are divisible by
+// 
+    for ( let x = 0; x < small; x++){
 
+        if (big % x === 0 && small % x === 0){
+            
+            arr.push(x);
+            
+        }
+        
+    }
+
+    return arr.pop();
 }
 
+console.log(gcd(35,70))
 module.exports = gcd;
