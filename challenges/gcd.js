de717d58e1873caef9fcaf206151d6250f0a41a9
nathanielgrossman <nategrossman@mac.com>
2018-07-13 15:33:31 -0700

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,15 @@
  */
 
 function gcd(a, b) {
-
+	let cd;
+	let i = 1;
+	while (i <= a || i <= b) {
+		if (a % i === 0 && b % i === 0) {
+			cd = i;
+		}
+		i++;
+	}
+	return cd;
 }
 
 module.exports = gcd;
