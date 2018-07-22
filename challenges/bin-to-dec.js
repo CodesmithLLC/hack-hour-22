d58330fa1b43b3a2cc40356a7da7a7405f7ac7a5
Mia <ngocmhuynh5@gmail.com>
2018-07-21 17:45:47 -0700

    :art: Added linebreak
    Reformatted alternate solution #1

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -16,6 +16,7 @@
 function binToDec(binary) {
     // let digit = parseInt(binary, 2);
     // return digit;
+    
     return binary.split('').reverse().reduce((x, y, i) => {
         return (y === '1') ? x + Math.pow(2, i) : x;
     }, 0);
