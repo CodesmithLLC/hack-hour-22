40f984ba07ef696cd9e792a2c751949b35f07809
Stephanie Fong <stfongg@gmail.com>
2018-07-23 09:40:53 -0700

    completed count-twos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -10,7 +10,18 @@
 
 
 function countTwos(num) {
-
+  // declare a counter variable
+  // loop through numbers from 1 to n
+  // convert number to string of characters
+  // if character === '2', increase count
+  let count = 0;
+  for (let i = 1; i <= num; i += 1) {
+    const s = i.toString().split('');
+    for (let j = 0; j < s.length; j += 1) {
+      if (s[j] === '2') count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = countTwos;
