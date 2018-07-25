905cba72de934903b74ea8efe9dce1951370fd30
Clariz Mariano <clariz.mariano@gmail.com>
2018-07-25 09:33:48 -0700

    late late late

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -11,14 +11,65 @@
  * The 8 is the hundreds digit (1 carried over + 5 + 2).
  *
  */
-
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
 function addLinkedList(l1, l2) {
+  let head1 = l1;
+  let head2 = l2;
+  // put everything in an array
+  // then pop the element and insert into an array 
+  const arr1 = [];
+  const arr2 = [];
+  while (head1){
+    arr1.push(head1.value);
+    head1 = head1.next;
+  }
+
+
+  while (head2){
+    arr2.push(head2.value);
+    head2 = head2.next;
+  }
+  // console.log(arr1, arr2)
+  
+  const sum = parseInt(arr1.reverse().join('')) + parseInt(arr2.reverse().join(''));
+  // console.log(sum)
+
+  // create the LL
+  // make the sum an array
+  const llContent = sum.toString().split('');
+  
+
+  const newTree = new Node(llContent.pop());
+  // console.log(newTree);
+  let head = newTree;
+  // console.log(head);
+  // console.log(llContent); 
+  
+  while (llContent[0]){
+    // console.log(llContent);
+    head.next = new Node(llContent.pop());
+    head = newTree.next;
+    // console.log(newTree);
+  }
+  
+  return newTree;
 
 }
 
+
+
+let tree1 = new Node(2);
+ tree1.next = new Node(1);
+ tree1.next.next = new Node(5);
+let tree2 = new Node(5);
+ tree2.next = new Node(9);
+ tree2.next.next = new Node(2);
+ console.log(addLinkedList(tree1, tree2));
+
+// console.log(tree1);
+// console.log(tree2);
 module.exports = {Node: Node, addLinkedList: addLinkedList};
