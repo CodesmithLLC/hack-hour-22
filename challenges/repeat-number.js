3823fe4889516aa95eb33138ecbaaca9d11aac12
Clariz Mariano <clariz.mariano@gmail.com>
2018-07-26 09:24:39 -0700

    repeat number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,18 @@
  */
 
 function repeatNumbers(array) {
-
+    const string = array.join('');
+    // \b\w*(\w{2})\w*\1
+    array = array.sort();
+    for (let x = 0; x < array.length; x++){
+        let check = array[x];
+        if (array[x+1] === array[x]){
+            return array[x]
+        }
+        
+    }
+    return;
 }
 
+console.log(repeatNumbers([1,2,9,3,4,5,6,7,8,9]));
 module.exports = repeatNumbers;
