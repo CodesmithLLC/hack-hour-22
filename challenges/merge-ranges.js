d4e5bc3ba1e7547f4f66bb9d6b2ff0d0c1f93e20
cssaavedra <cssaavedra56@gmail.com>
2018-07-27 10:14:36 -0700

    merged arrays completed

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,27 @@
 
 
 function mergeRanges(array) {
-
+    
+    for (let outerI = array.length; outerI > 0; outerI-= 1){
+        if (array[outerI]){
+          for (let innerI = outerI -1; innerI > 0; innerI-= 1){
+            console.log(array[outerI]);
+            console.log(array[innerI]);
+            if (array[innerI][0] > array[outerI][0] && array[innerI][0] <= array[outerI][1]){
+              array[outerI] = [Math.min(array[outerI][0],array[innerI][0]), Math.max(array[innerI][1], array[outerI][1])]  
+              array[innerI] = undefined;
+            }
+            else if (array[innerI][1] > array[outerI][0] && array[innerI][1] <= array[outerI][1]){
+              array[outerI] = [Math.min(array[outerI][0],array[innerI][0]), Math.max(array[innerI][1], array[outerI][1])]  
+              array[innerI] = undefined;
+              }
+        }
+        }
+    }
+    const mergedTimes = array.filter((element) =>{
+      if (element !== undefined) return element;
+      });
+      return mergedTimes;
 }
 
 module.exports = mergeRanges;
