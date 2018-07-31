3b08e1f20480bfef243e496456a7136aa3e3d968
Erik Cox <erikbcox@gmail.com>
2018-07-31 09:52:39 -0700

    Add solution

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -7,8 +7,20 @@
  *
  */
 
-function maxSubarray(arr) {
+const maxSubArray = (arr) => {
+  let maxn = -Number.MAX_VALUE;
+  let sum = 0;
 
-}
+  arr.forEach((item, index, array) => {
+    sum += item;
+    if (sum > maxn) maxn = sum;
+    if (sum < 0) sum = 0;
+  });
+  return maxn;
+};
 
 module.exports = maxSubarray;
+
+// Tests
+// console.log("18: " + maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
+// console.log("40: " + maxSubarray([15,20,-5,10]));
