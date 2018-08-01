19e40ab41b7e04e989486786bb96cd2c21f5efe5
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2018-08-01 09:34:15 -0700

    Complete Anagrams challenge

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,26 @@
   */
 
 function anagrams(string) {
-
+  const strArr = string.split('');
+  const anagramArr = [];
+  let comboArray;
+  for (let i = 0; i < strArr.length; i += 1) {
+    comboArray = [strArr[i]];
+    for (let j = 0; j < strArr.length; j += 1) {
+      if (i !== j) {
+        comboArray.push(strArr[j]);
+      }
+    }
+    anagramArr.push(comboArray.join(''));
+    comboArray = [strArr[i]];
+    for (let k = (strArr.length - 1); k >= 0; k -= 1) {
+      if (i !== k) {
+        comboArray.push(strArr[k]);
+      }
+    }
+    anagramArr.push(comboArray.join(''));
+  }
+  return anagramArr;
 }
 
 module.exports = anagrams;
