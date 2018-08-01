76de3e90fd3b35f256c66b66a8936c7f1dfd0c54
nathanielgrossman <nategrossman@mac.com>
2018-08-01 11:33:39 -0700

    ana

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,27 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
-
+function permutations(arr, length, word = [], output = []) {
+	let temp;
+	for (let i = 0; i < arr.length; i++) {
+		temp = arr.slice();
+		word.push(...temp.splice(i, 1));
+		if (word.length === length) {
+			output.push(word.join(''));
+		} else {
+			output.push(...permutations(temp, length, word, output));
+		}
+		word = [];
+	}
+	return output;
 }
 
-module.exports = anagrams;
+function anagrams(string) {
+	const arr = string.split('')
+	let anagrams = permutations(arr, arr.length);
+	let unique = new Set(anagrams);
+	return Array.from(unique)
+}
+
+module.exports = anagrams, permutations;
+
