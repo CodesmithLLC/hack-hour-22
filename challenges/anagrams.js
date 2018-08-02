3a5eabd76f949f479d732963e916f1c9b44a07c9
Clariz Mariano <clariz.mariano@gmail.com>
2018-08-02 09:44:03 -0700

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -11,9 +11,51 @@
   * var result = anagrams('abc');
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
+ 
+ function anagrams(string) {
+  let anagrams = [];
+  // extract first element. 
+  // put the other characters in an array
+  // permutate over those strings... 
 
-function anagrams(string) {
+  string = string.split('');
 
+  
+  console.log(string);
+
+
+  let resultArr = [];
+  let copy = Object.assign(string);
+  console.log(copy)
+  // do the anagram thing here
+
+  copy.forEach((firstelement, index, array) => {
+    
+    console.log(firstelement, array());
+
+
+  });
+
+
+  // for (let x = 0; x < string.length; x++){
+  //   copy = copy.slice(1);
+  //   console.log(x, string[x], copy);
+  //   let newWord;
+    
+  //   string.reduce( (e, i) => {
+  //     newWord = 0;
+  //   }, 0)
+  // }
+
+  // join the string
+  // push it to the result array
+
+
+  // return array
+
+  return anagrams;
 }
+
+console.log(anagrams('abc'));
 
 module.exports = anagrams;
--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -14,6 +14,44 @@
 
 function deleteDups(head) {
 
+    // traverse over linkedlist
+    let pointer = head;
+    let store = {};
+    let previous;
+    while (pointer){
+    //   console.log(pointer.value, " = value || previous =", previous);
+
+      if (!store[pointer.value]) {
+        // store value in an object
+        store[pointer.value] = 'I exist';
+      }
+      else {
+        // if the item is in the storage obj, delete it
+        // console.log('yo, this is a duplicate')
+        previous.next = pointer.next;
+      }
+      previous = pointer;
+      pointer = pointer.next;
+
+
+    }
+    // console.log(store);
+    // console.log("HEAD", head);
+    return head;
 }
 
+function Node(val) {
+    this.value = val;
+    this.next = null;
+}
+
+
+
+ let linkedList =  new Node(1);
+ linkedList.next =  new Node(1);
+ linkedList.next.next =  new Node(3);
+ linkedList.next.next.next =  new Node(4);
+//  console.log(deleteDups(linkedList))
+
+//  console.log(linkedList);
 module.exports = deleteDups;
