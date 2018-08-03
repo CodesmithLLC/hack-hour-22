9bd1ea0b1e418005dc6dd22ded3fc5104cba406f
Sam Goldberg <sgoldber61@gmail.com>
2018-08-03 09:41:27 -0700

    delete dups and event emitter

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,26 @@
 
 
 function deleteDups(head) {
-
+  if (!head) return null;
+  
+  const resultHead = {value: head.value, next: null};
+  let curr = head.next;
+  let resultCurr = resultHead;
+  
+  const uniqueVals = new Set(head.value);
+  
+  while (curr) {
+    if (!uniqueVals.has(curr.value)) {
+      uniqueVals.add(curr.value);
+      
+      resultCurr.next = {value: curr.value, next: null};
+      resultCurr = resultCurr.next;
+    }
+    
+    curr = curr.next;
+  }
+  
+  return resultHead;
 }
 
 module.exports = deleteDups;
