e1e0ec2a654d7f2d5cf1e5c202cfc7329639ca79
nathanielgrossman <nategrossman@mac.com>
2018-08-03 09:18:30 -0700

    event

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,21 @@
  */
 
 function EventEmitter() {
-
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+	if (!this.funcName) {
+		this.funcName = [func]
+	} else {
+		this.funcName.push(func)
+	}
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+	this.funcName.forEach(func => {
+		func(...args);
+	})
 };
 
 module.exports = EventEmitter;
+
