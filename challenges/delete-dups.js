fcb9cf8e87cd5b247a28a4c1e522cfcea6272491
Joe Thel <jgraythel@gmail.com>
2018-08-03 09:39:33 -0700

    🔄 delete dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,31 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
+function LinkedList(value) {
+  this.value = value;
+  this.next = null;
+}
+
+function test() {
+  const a = new LinkedList(1);
+  a.next = new LinkedList(2);
+  a.next.next = new LinkedList(3);
+  a.next.next.next = new LinkedList(3);
+  return a;
+}
 
 
 function deleteDups(head) {
-
+  let current = head;
+  const seen = [];
+  while (current) {
+    seen.push(current.value);
+    if (seen.includes(current.next.value)) {
+      current.next = current.next.next;
+    }
+    current = current.next;
+  }
+  return head;
 }
 
-module.exports = deleteDups;
+module.exports = { deleteDups, test, LinkedList };
