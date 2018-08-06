f6e2af75049b784cd869061be3d79fd932b536e3
Mia <ngocmhuynh5@gmail.com>
2018-08-06 09:28:21 -0700

    :page_facing_up: insertionSort
        #1

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,16 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 0; i < array.length; i += 1) {
+    let temp = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j -= 1;
+    }
+    array[j + 1] = temp;
+  }
+  return array;
 }
 
 module.exports = insertionSort;
