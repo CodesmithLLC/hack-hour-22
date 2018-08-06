076157b26e77996ede5e2f1388a366656e48fd37
Joe Thel <jgraythel@gmail.com>
2018-08-06 09:16:24 -0700

    🎆 insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -1,11 +1,23 @@
+/* eslint no-param-reassign: 0 */
 // Write a function that sorts an array using the insertion sort algorithm.
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
+const swap = (array, a, b) => {
+  [array[a], array[b]] = [array[b], array[a]];
+};
 
 function insertionSort(array) {
+  if (!Array.isArray(array) || array.length < 2) return array;
 
+  for (let i = 1; i < array.length; i += 1) {
+    for (let j = i; j >= 1 && array[j] < array[j - 1]; j -= 1) {
+      swap(array, j, j - 1);
+    }
+  }
+
+  return array;
 }
 
 module.exports = insertionSort;
