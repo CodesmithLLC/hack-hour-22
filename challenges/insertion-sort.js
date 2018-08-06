429ac755ba1517fb1595515ef10ce71814b720d0
Sam Goldberg <sgoldber61@gmail.com>
2018-08-06 09:58:00 -0700

    insertion sort incomplete

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,19 @@
 
 
 function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    // insert(array, )
+  }
+}
 
+function insert(array, target, insert) {
+  let i = insert;
+  while (i > target)
+    swap(array, i - 1, i);
+}
+
+function swap(array, i, j) {
+  [array[i], array[j]] = [array[j], array[i]];
 }
 
 module.exports = insertionSort;
