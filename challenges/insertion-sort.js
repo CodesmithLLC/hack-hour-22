4c3708619f6fa8c816bc32d9e86c59724cfed608
nathanielgrossman <nategrossman@mac.com>
2018-08-06 09:50:42 -0700

    insort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,23 @@
 
 
 function insertionSort(array) {
-
+    for (let i = 1; i < array.length; i++) {
+      for (let j = i - 1; j >= 0; j--) {
+        if (array[i] < array[j]) {
+          if (j === 0) {
+            let temp = array.splice(i, 1);
+            array.splice(0, 0, ...temp);
+            break;  //use splice to insert item at 0
+          }
+          if (array[j - 1] <= array[i]) {
+            let temp = array.splice(i, 1);
+            array.splice(j, 0, ...temp);
+            break;
+          }
+        }
+      }
+    }
+    return array;
 }
 
 module.exports = insertionSort;
