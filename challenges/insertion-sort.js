350cb440ed2b252381fec513e8dbbdb178f6e41d
Sam Goldberg <sgoldber61@gmail.com>
2018-08-07 09:27:55 -0700

    insertion sort complete

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,19 +5,30 @@
 
 
 function insertionSort(array) {
-  for (let i = 0; i < array.length; i++) {
-    // insert(array, )
+  for (let j = 1; j < array.length; j++) {
+    for (let i = 0; i < j; i++) {
+      if (array[j] < array[i])
+        insert(array, i, j);
+    }
   }
+  
+  return array;
 }
 
 function insert(array, target, insert) {
   let i = insert;
-  while (i > target)
+  while (i > target) {
     swap(array, i - 1, i);
+    i--;
+  }
 }
 
 function swap(array, i, j) {
   [array[i], array[j]] = [array[j], array[i]];
 }
+
+console.log(insertionSort([-40, 20, 4, 2, -80, 1, 5, 3, 0]));
+console.log(insertionSort([-40, 20, 4, 2, -80, 6, 1, 5, 3, 0]));
+
 
 module.exports = insertionSort;
