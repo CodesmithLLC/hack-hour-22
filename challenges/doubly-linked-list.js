0fc0090748ac688d1326f7ad2423736b8830c27f
Ryan Trontz <rtrontz@gmail.com>
2018-08-09 09:22:42 -0700

    quick commit

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,22 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  const newNode = new Node(val);
+  newNode.prev = this.tail;
+  this.tail.next = newNode;
+  this.tail = newNode;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let currentNode = this.head
+  while (currentNode.val !== val) {
+    currentNode = currentNode.next
+  }
+  currentNode.prev.next = currentNode.next;
+  currentNode.next.prev = current.node.prev;
 };
 
 module.exports = LinkedList;
