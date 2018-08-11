b73ae6a602748959f33584ccd4288bc049e6c2da
briantaylor <brian.taylor818@gmail.com>
2018-08-11 10:23:35 -0700

    bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,16 @@
 
 
 function bubbleSort(array) {
-
+  for (let i = 0; i < array.length; i++) {
+    for (j = 0; j < array.length - 1; j++) {
+      if(array[j] > array[j+1]) {
+        const smaller = array[j+1];
+        array[j+1] = array[j];
+        smaller = array[j];
+      }
+    }
+  } 
+  return smaller;
 }
 
 module.exports = bubbleSort;
