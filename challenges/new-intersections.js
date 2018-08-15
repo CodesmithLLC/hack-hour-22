2dce090281bfe23543e142cfa55bd86ae9642f0f
Sam Goldberg <sgoldber61@gmail.com>
2018-08-15 09:54:08 -0700

    new intersections

--- challenges/new-intersections.js
+++ challenges/new-intersections.js
@@ -17,8 +17,58 @@
  * 	 
  */
 
-function newIntersections(x, y){
-
+function newIntersections(x, y) {
+  const xLines = {}, yLines = {};
+  
+  // gather line segment data
+  for (let i = 0; i < x.length; i++) {
+    const X = x[i], Y = y[i];
+    
+    // add to xLines
+    if (Array.isArray(xLines[Y])) { // array of two elements: lower and upper bound
+      if (X < xLines[Y][0]) xLines[Y][0] = X;
+      else if (X > xLines[Y][1]) xLines[Y][1] = X;
+    }
+    else if (xLines.hasOwnProperty(Y)) { // single number
+      const X_curr = xLines[Y];
+      xLines[Y] = [Math.min(X, X_curr), Math.max(X, X_curr)];
+    }
+    else // not defined yet
+      xLines[Y] = X;
+    
+    // add to yLines
+    if (Array.isArray(yLines[X])) { // array of two elements: lower and upper bound
+      if (Y < yLines[X][0]) yLines[X][0] = Y;
+      else if (Y > yLines[X][1]) yLines[X][1] = Y;
+    }
+    else if (yLines.hasOwnProperty(X)) { // single number
+      const Y_curr = yLines[X];
+      yLines[X] = [Math.min(Y, Y_curr), Math.max(Y, Y_curr)];
+    }
+    else // not defined yet
+      yLines[X] = Y;
+  }
+  
+  // look over the created xLines and yLines, and determine intersections
+  let total = 0;
+  for (const yVal in xLines) {
+    for (const xVal in yLines) {
+      const xLine = xLines[yVal], yLine = yLines[xVal];
+      
+      // is the x value of the yLine within the xLine, and the y value of the xLine within the yLine?
+      total += (xLine[0] < xVal) && (xVal < xLine[1]) && (yLine[0] < yVal) && (yVal < yLine[1]);
+    }
+  }
+  
+  return total;
 }
+
+// let x = [2, 3, 1, 4, 0, 2, 3, 1];
+// let y = [-1, -1, 0, 0, 1, 1, 1, 2];
+// console.log(newIntersections(x, y));
+// 
+// x = [2, 3, 1, 4, 0, 2, 3, 1];
+// y = [0, -1, 0, 0, 1, 1, 1, 2];
+// console.log(newIntersections(x, y));
 
 module.exports = newIntersections;
