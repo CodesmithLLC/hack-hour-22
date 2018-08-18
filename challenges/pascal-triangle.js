131ddc1df0eb42b0c9500d8e9a0bd21932da8b00
Clariz Mariano <clariz.mariano@gmail.com>
2018-08-18 10:29:24 -0700

    pascal triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -34,6 +34,56 @@
 
 function pascalTriangle(numRows) {
 
+  const resultArray = [];
+  let arrayHolder;
+  let prevArray;
+
+  // create a loop while numRows is not 0; decrement each time
+  do {
+
+    if (resultArray.length === 0 ){
+      arrayHolder = [1]
+    }
+    else if (resultArray.length === 1 ){
+      arrayHolder = [1, 1]
+    }
+    else if (resultArray.length > 1 ){
+      arrayHolder = [1];
+      // do loop logic here that pushes the sum of adjacent elements to the middle of arrayHolder
+
+      prevArray.forEach((e,i )=> {
+        if (i !== prevArray.length -1) {
+          console.log(prevArray[i], prevArray[i+1], " = ", prevArray[i] + prevArray[i+1])
+          arrayHolder.push(parseInt(prevArray[i]) + parseInt(prevArray[i+1]));
+        }
+        
+      });
+
+      arrayHolder.push(1);
+
+    }
+    
+
+    numRows -= 1;
+    resultArray.push(arrayHolder);
+    prevArray =JSON.parse(JSON.stringify(arrayHolder))
+
+    // console.log(numRows,resultArray, "======", prevArray );
+
+
+  } while (numRows)
+
+
+  return resultArray;
 }
 
+console.log(pascalTriangle(6))
+// EXPECTED:
+// [
+//   [1],
+//   [1,1],
+//   [1,2,1],
+//   [1,3,3,1]
+
+// ]
 module.exports = pascalTriangle;
