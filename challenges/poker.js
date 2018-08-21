59f85e9bfe060cd84737f7f27eb6a4f1b5a7e7a8
Joe Thel <jgraythel@gmail.com>
2018-08-21 09:43:16 -0700

    💰💰💰 poker!

--- challenges/poker.js
+++ challenges/poker.js
@@ -15,12 +15,78 @@
  *
  * Clearly real-world poker has more complex rules for tied ranks, but we want you
  * to focus on overall architecture rather than edge cases. Have fun!
- * 
+ *
  * BONUS: Account for suits and add in Flush & Straight Flush/Royal Flush.
  * BONUS2: Create a deck of cards function that generates two random hands for you.
  */
-function poker(hand1, hand2) {
 
+function countCards(hand) {
+  const count = {};
+  hand.forEach((card) => {
+    count[card] = count[card] + 1 || 1;
+  });
+  return count;
 }
+
+const ranks = [
+  count => Object.values(count).includes(4),
+  count => Object.entries(count).length === 2,
+  (count) => {
+    const cards = Object.keys(count).sort();
+    return (
+      cards[4] - cards[3] === 1 &&
+      cards[3] - cards[2] === 1 &&
+      cards[2] - cards[1] === 1 &&
+      cards[1] - cards[0] === 1
+    );
+  },
+  count => Object.values(count).includes(3),
+  count => Object.values(count).filter(num => num === 2).length === 2,
+  count => Object.values(count).includes(2),
+];
+
+function until(array, item) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i](item)) return i;
+  }
+  return array.length;
+}
+
+function poker(...hands) {
+  const [oneRank, twoRank] = hands
+    .map(countCards)
+    .map(count => until(ranks, count));
+  if (oneRank !== twoRank) {
+    return oneRank < twoRank
+      ? 'Player 1 wins'
+      : 'Player 2 wins';
+  }
+
+  const [oneHigh, twoHigh] = hands.map(hand => Math.max(...hand));
+  if (oneHigh !== twoHigh) {
+    return oneHigh > twoHigh
+      ? 'Player 1 wins'
+      : 'Player 2 wins';
+  }
+
+  return 'Draw';
+}
+
+function Deck() {
+  this.size = 52;
+  this.deck = Array(52).fill(null).map((val, idx) => {
+    return Math.floor((idx / 4) + 2);
+  });
+}
+
+Deck.prototype.deal = function deal() {
+  const hand = [];
+  for (let i = 0; i < 5; i += 1) {
+    const card = Math.random() * this.size;
+    hand.push(...this.deck.splice(card, 1));
+    this.size -= 1;
+  }
+  return hand.sort((a, b) => a - b);
+};
 
 module.exports = poker;
