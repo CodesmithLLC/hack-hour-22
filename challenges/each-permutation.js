411e120ee91b3321643255ce5aee393decf996e9
Mia <ngocmhuynh5@gmail.com>
2018-08-29 10:11:09 -0700

    one more try

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -42,16 +42,3 @@ const eachPermutation = (arr) => {
 
 
 module.exports = eachPermutation;
-
-
-
-
-
-try {
-    // Node.js code generating an Error.
-} catch (error) {
-    if (error.message === 'ERR_HTTP_INVALID_STATUS_CODE') {
-        // Perform some logic.
-    }
-}
- 
